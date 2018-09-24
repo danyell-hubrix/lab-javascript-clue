7bb268554a6db78bd2fb463e7350648f5b46451a
DdSG <daniel@hubrix.co>
2018-09-24 21:40:08 +0200

    Still messy but basically there

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -67,7 +67,7 @@ name: trophy      ---   weight: 25
 name: pistol      ---   weight: 20
 
 // Rooms
-name: Dinning Room
+name: Dining Room
 name: Conservatory
 name: Kitchen
 name: Study
@@ -84,11 +84,106 @@ name: Guest House
 name: Patio
 */
 
+
 // Characters Collection
-var charactersArray = [];
+var charactersArray = {
+    'MrGreen': {
+        first_name:   "Jacob",
+        last_name:    "Green",
+        color:        "green",
+        description:  "He has a lot of connections",
+        age:          45,
+        image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        occupation:   "Entrepreneur"
+        }
+    , 'drOrchid': {
+        frst_name:   'Doctor',
+        lst_name:    'Orchid',
+        color:        'white',
+        description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        age:          26,
+        image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        ocupation:   'Scientist'
+    }
+    , 'ProfPlum': {
+        first_name:  "Victor",
+        last_name:   "Plum",
+        color:       "purple",
+        description: "Billionare video game designer",
+        age:         22,
+        image:       "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        occupation:  "Designer"
+    }
+    , 'missScarlet': {
+        first_name:  "Kasandra",
+        last_name:   "Scarlet",
+        color:       "red",
+        description: "She is an A-list movie star with a dark past",
+        age:         31,
+        image:       "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation:  "Actor"
+    }
+};
 
 // Rooms' Collection
-var roomsArray = [];
+var weaponsArray = [
+   { name:'rope',           weight: 10 },
+   { name:'knife',          weight: 8 },
+   { name:'candlestick',    weight: 2 },
+   { name:'dumbbell',       weight: 30 },
+   { name:'poiso',          weight: 2 },
+   { name:'axe',            weight: 15 },
+   { name:'bat',            weight: 13 },
+   { name:'trophy',         weight: 25 },
+   { name:'pistol',         weight: 20 }
+];
 
 // Weapons Collection
-var weaponsArray = [];
+var roomsArray = [
+    { name:'Dining Room' },
+    { name:'Conservatory' },
+    { name:'Kitchen' },
+    { name:'Study' },
+   { name:'Library' },
+   { name:'Billiard Room' },
+   { name:'Lounge' },
+   { name:'Ballroom' },
+   { name:'Hall' },
+   { name:'Spa' },
+   { name:'Living Room' },
+   { name:'Observatory' },
+   { name:'Theater' },
+   { name:'Guest House' },
+   { name:'Patio' }
+];
+
+function randomSelector(sourceArray) {
+    if (typeof(sourceArray) != 'object') return undefined;
+    var numberOfKeys = sourceArray.length;
+    if (0 == numberOfKeys) return undefined;
+    var keysArr = Object.keys(sourceArray);
+    var rndIndx = Math.floor(Math.random() * numberOfKeys);
+    return sourceArray[keysArr[rndIndx]];
+}
+
+function pickMistery() {
+    var nastyPerson = randomSelector(charactersArray);
+    var weapon = randomSelector(weaponsArray);
+    var location = randomSelector(roomsArray);
+    var responseArray = [
+        { first_name: nastyPerson.first_name, last_name: nastyPerson.last_name },
+        { name: weapon.name },
+        { name: location.name }
+        ];
+    return responseArray;
+}
+
+function revealMistery(mistyEnvelope) {
+    return
+        mistyEnvelope[0].first_name + " "
+        + mistyEnvelope[0].last_name +
+        " killed Mr.Boddy using the " +
+        mistyEnvelope[1].name +" in the "
+        + mistyEnvelope[2].name + "!!!!"
+    ;
+}
